00d53ab86a8cac0ed121b338f52860592ce136c1
Ahmed Elmayyah <a.elmayyah@gmail.com>
2018-12-20 14:01:16 +0200

    Update 1.0
    Added StringCompressor class that take an ASCII string of 1s and 0s and convert it to an ASCII string of characters by grouping every 8 characters into one by using the decimal value represented by the 8 characters and vice versa.

---
+++ stringCompressor.h
@@ -0,0 +1,13 @@
+#include <string>
+#include <math.h>
+
+using std::string;
+
+class StringCompressor
+{
+public:
+	string decimalToBinaryString(int x, int nBits);
+	int binaryStringtoDecimal(string s);
+	string binaryStreamtoASCII(string encodedString);
+	string asciitoBinaryStream(string asciiString);
+};
